76821a335f2cc671b9027a85912819cd580b9b5d
YanChan  Lin <yonalin1@gmail.com>
2017-11-01 00:06:57 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,6 @@
 require 'pry'
 def key_for_min_value(name_hash)
   smallest_value = nil
-  smallest_key= nil
   name_hash.each do |key,value|
     if smallest_value == nil
       smallest_value = value
@@ -14,6 +13,5 @@ def key_for_min_value(name_hash)
       smallest_value = value
     end
   end
-  binding.pry
  smallest_key
 end
