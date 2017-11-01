d97c67314d1778a91110f8a0f6beb971927af942
YanChan  Lin <yonalin1@gmail.com>
2017-11-01 00:08:37 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 require 'pry'
 def key_for_min_value(name_hash)
   smallest_value = nil
-  smallest_key = nil 
+  smallest_key = nil
   name_hash.each do |key,value|
     if smallest_value == nil
       smallest_value = value
